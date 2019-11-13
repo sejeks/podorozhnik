eb03312963e2c13a69b9c5ea452053f2ab2d9efd
Sergei <sejeks@gmail.com>
2019-11-13 14:27:16 +0300

    hihihih

--- app/src/App.js
+++ app/src/App.js
@@ -6,11 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>Use this tool to calculate how much money had you spent in this month by transport.</p>
-      <p>Also use app <a target='blank' href="https://play.google.com/store/apps/details?id=org.plantainreader&hl=ru" >Plantain reader</a> to see how many times use used you card.</p>
+        <p>Use this tool to calculate how much money had you spent in this month by transport.</p>
+        <p>Also use app <a target='blank' href="https://play.google.com/store/apps/details?id=org.plantainreader&hl=ru" >
+          Plantain reader</a> to see how many times use used you card.</p>
 
-        <Transport typeOfTransport="Метро" basePrice={ 40 } />
-        <Transport typeOfTransport="Наземный" basePrice={ 35 } />
+        <Transport typeOfTransport="Метро" basePrice={40} />
+        <Transport typeOfTransport="Наземный" basePrice={35} />
+        
       </header>
     </div>
   );
